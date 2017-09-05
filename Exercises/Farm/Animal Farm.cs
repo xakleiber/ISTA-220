043bfa09d12a15040ad798b9288e1607d204b6fe
Xavier Kleiber <30834460+xakleiber@users.noreply.github.com>
2017-09-04 23:04:37 -0400

    Add files via upload

---
+++ Exercises/Farm/Animal Farm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chicken ch = new Chicken();
+            ch.whatAmI("chicken");
+            ch.speak("Bok Bok");
+
+            Cow cow = new Cow();
+            cow.whatAmI("cow");
+            cow.speak("moo");
+
+            Goat goat = new Goat();
+            goat.whatAmI("goat");
+            goat.speak("Baaaaah");
+
+            Pig pig = new Pig();
+            pig.whatAmI("pig");
+            pig.speak("oink");
+        }
+    }
+}
